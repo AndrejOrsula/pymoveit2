ea4b731fc8a987447e6c612cc69c83b340686cca
Markus Grotz <grotz@uw.edu>
2024-04-23 23:20:44 -0700

    add geodude robot

---
+++ pymoveit2/robots/geodude_left.py
@@ -0,0 +1,37 @@
+from typing import List
+
+MOVE_GROUP_ARM: str = "left_arm"
+MOVE_GROUP_GRIPPER: str = "left_hand"
+
+OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [0.0, 0.0, 0.0, 0.0]
+CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [1.04, 1.04, 1.04, 1.04]
+
+
+def joint_names(prefix: str = "left_") -> List[str]:
+    return [
+        prefix + "j1",
+        prefix + "j2",
+        prefix + "j3",
+        prefix + "j4",
+        prefix + "j5",
+        prefix + "j6",
+        prefix + "j7",
+    ]
+
+
+def base_link_name(prefix: str = "left_") -> str:
+    return prefix + "wam1"
+
+
+def end_effector_name(prefix: str = "left_") -> str:
+    return prefix + "hand_tcp"
+
+
+def gripper_joint_names(prefix: str = "left_") -> List[str]:
+    return [
+        prefix + "j01",
+        prefix + "j11",
+        prefix + "j21",
+        prefix + "j00"]
+    
+
---
+++ pymoveit2/robots/geodude_right.py
@@ -0,0 +1,36 @@
+from typing import List
+
+MOVE_GROUP_ARM: str = "right_arm"
+MOVE_GROUP_GRIPPER: str = "right_hand"
+
+OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [0.0, 0.0, 0.0, 0.0]
+CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [1.04, 1.04, 1.04, 1.04]
+
+
+def joint_names(prefix: str = "right_") -> List[str]:
+    return [
+        prefix + "j1",
+        prefix + "j2",
+        prefix + "j3",
+        prefix + "j4",
+        prefix + "j5",
+        prefix + "j6",
+        prefix + "j7",
+    ]
+
+
+def base_link_name(prefix: str = "right_") -> str:
+    return prefix + "wam1"
+
+
+def end_effector_name(prefix: str = "right_") -> str:
+    return prefix + "hand_tcp"
+
+
+def gripper_joint_names(prefix: str = "right_") -> List[str]:
+    return [
+        prefix + "j01",
+        prefix + "j11",
+        prefix + "j21",
+        prefix + "j00",
+    ]
